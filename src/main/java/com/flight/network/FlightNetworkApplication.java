b6c1236449966441b3c4e2e6401dcc9f85bd28e7
laxminarayan <laxminarayan.lexicon@gmail.com>
2018-10-13 13:18:17 +0530

    first commit

---
+++ src/main/java/com/flight/network/FlightNetworkApplication.java
@@ -0,0 +1,12 @@
+package com.flight.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightNetworkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flight/network/FlightNetworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flight.network;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightNetworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
